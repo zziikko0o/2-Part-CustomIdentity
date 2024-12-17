304c3a298fc70ad48490d10006c46430f43c8bbd
zziikko0o <zziikko0o@gmail.com>
2024-12-17 14:54:15 +0100

    test2Commit

--- obj/Release/net8.0/CRUDWithWebApi.AssemblyInfo.cs
+++ obj/Release/net8.0/CRUDWithWebApi.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("CRUDWithWebApi")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10ed939a7c675735d66f72021bbb3306516c5c30")]
 [assembly: System.Reflection.AssemblyProductAttribute("CRUDWithWebApi")]
